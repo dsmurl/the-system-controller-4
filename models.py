b205f94fd2dc567f73dc78968b616de192804b56
Samuel Bagot <dsmurl@gmail.com>
2015-08-31 00:09:51 -0500

    #21 ActionsOnRules    fixed missing code from merge.

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import logging
 import datetime
 import json
+import gevent
 from peewee import *
 from playhouse.shortcuts import model_to_dict
 from lib import utils, gpio, evaluator
@@ -12,6 +13,7 @@ class BaseModel(Model):
     class Meta:
         database = utils.get_db()
 
+    # Returns the Entity with the given id
     @classmethod
     def get_by_id(cls, id):
         try:
@@ -19,10 +21,13 @@ class BaseModel(Model):
         except cls.DoesNotExist:
             return None
 
+    # Converts this entity to a dictionary to be passed around
     def to_client(self):
 
         return model_to_dict(self)
 
+    # Creates a key string that includes this entities name,
+    # id, and a possible property_or_method.  All separated by /.
     def key(self, property_or_method=None):
         args = [self.__class__.__name__, self.id]
 
@@ -31,6 +36,9 @@ class BaseModel(Model):
 
         return '/'.join(map(lambda arg: str(arg), args))
 
+    # Retrieves an entity by it's key like "Sensor/1" or
+    # getting a value of a key of this entity by something like
+    # "Sensor/1/value"
     @classmethod
     def get_by_key(cls, key, default=None):
         if str(key).count('/') > 0:
@@ -61,8 +69,12 @@ class Sensor(BaseModel):
     pin = CharField(index=False)
 
     def value(self):
-        # TODO implement sensor value get
-        return '256'
+
+        logging.debug("Running read_sensor " + self.pin + "...")
+        reading = gpio.read(self.pin)
+        logging.debug("Done with read_sensor.  " + self.pin + " =>  " + str(reading))
+
+        return reading
 
     def to_client(self):
 
@@ -168,6 +180,19 @@ class Rule(BaseModel):
         data['actions'] = self.get_actions()
 
         return data
+
+    @classmethod
+    def background_run_rules(cls):
+        from lib import evaluator
+
+        while True:
+            rules = cls.select()
+
+            for rule in rules:
+                eval_rule = evaluator.Evaluate(rule)
+                eval_rule.evaluate()
+
+            gevent.sleep(1)  # todo we can make the sleep system settings
 
 
 """
